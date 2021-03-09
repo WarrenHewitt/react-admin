6c4282a1a9e8c31e2715faaeaa1323c92a7e7cb6
hewei <heweimailbox@foxmail.com>
2021-03-09 23:07:05 +0800

    regular update

--- src/pages/pageStore/pageStore.js
+++ src/pages/pageStore/pageStore.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import { useSelector }  from 'react-redux'
+import { Button } from 'antd'
+import { useSelector, useDispatch }  from 'react-redux'
 
-import { selectCount  } from '@/store/slice/rootReducer'
+import { selectCount, changeName, add, addAnyNumber, subtractAsync  } from '@/store/slice/rootReducer'
 
 function PageStore () {
     const count =  useSelector(selectCount)
+    const dispatch =  useDispatch()
     const name =  useSelector(state => state.storeReducer.name)
+
+    const changeNameFn = () => {
+        dispatch(changeName())
+    }
+
+    const addFn = () => {
+        dispatch(add())
+    }
+
+    const addAnyFn = () => {
+        dispatch(addAnyNumber(10))
+    }
+
+    const addAnyAsyncFn = () => {
+        dispatch(subtractAsync(5))
+    }
 
     return (<div>
         <h1>pageStore</h1>
         <h1>直接在页面中获取:{ name }</h1>
         <h1>用预先定义的 selector 获取:{ count }</h1>
+        <Button type="primary" onClick={ addFn }>数字加一</Button>
+        <Button type="primary" onClick={ addAnyAsyncFn }>数字异步加5</Button>
+        <Button type="primary" onClick={ addAnyFn }>数字加10</Button>
+        <Button onClick={ changeNameFn }>修改名称</Button>
     </div>)
 
 }
--- src/store/slice/rootReducer.js
+++ src/store/slice/rootReducer.js
@@ -9,17 +9,35 @@ export const rootSlice  = createSlice({
     reducers: {
         add: (state) => {
             // 这里是因为使用了Immer库，所以能够使用这种直接修改state的语法，但其实并不是mutate
+            // 官方说明： Redux Toolkit allows us to write "mutating" logic in reducers. It
+            // doesn't actually mutate the state because it uses the Immer library,
+            // which detects changes to a "draft state" and produces a brand new
+            // immutable state based off those changes
             state.count += 1;
         },
-        changeName: (state) => {
+        changeName(state) {
             state.name = 'warren'
-        }      
+        },
+        addAnyNumber: (state, action) => {
+            console.log(action);
+            // { payload: 10, type: "testSlice/addAnyNumber" }
+            state.count += action.payload
+        },
     }
 })
 
-// https://juejin.cn/post/6844904129178009613#heading-7
+export const { add, changeName, addAnyNumber } = rootSlice.actions;
 
-export const { add, changeName } = rootSlice.actions;
+/* 异步处理 */
+// The function below is called a thunk and allows us to perform async logic. It
+// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
+// will call the thunk with the `dispatch` function as the first argument. Async
+// code can then be executed and other actions can be dispatched
+export const subtractAsync = number => dispatch =>  {
+    setTimeout(() => {
+        dispatch(addAnyNumber(number))
+    }, 1000);
+}    
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
@@ -27,8 +45,6 @@ export const { add, changeName } = rootSlice.actions;
 /* 这里的 storeReducer  根据 store.js 中定义而定 */
 /* 这里也可以不先定义  直接在页面中获取 */
 export const selectCount = (state) => state.storeReducer.count;
-
-// export const selectAll = (state) => state;
 
 export default rootSlice.reducer;
 
